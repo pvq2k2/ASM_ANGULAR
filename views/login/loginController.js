478d1173696f8de5bf009f14111f117de7ac1e40
pvq2k2 <vanquyet2392002@gmail.com>
2024-04-05 01:55:56 +0700

    validate form login and register

--- views/login/loginController.js
+++ views/login/loginController.js
@@ -12,25 +12,33 @@ myApp.controller("LoginController", function ($scope, $http, $location) {
 
   $scope.handleLogin = async () => {
     try {
-      await $http.get(apiUrl).then((res) => {
-        if (res.status == 200) {
-          const user = res.data.find(
-            (user) =>
-              $scope.user.username === user.username &&
-              $scope.user.password === user.password
-          );
-          if (user) {
-            let data = res.data[0];
-            delete data.password;
-            localStorage.setItem("user", JSON.stringify(data));
-            alert("Đăng nhập thành công !");
-            $location.path("/");
-          } else {
-            alert("Email hoặc mật khẩu không chính xác !");
-            return;
+      var invalid = false;
+      if ($scope.user.email != "" && $scope.user.password) {
+        invalid = true;
+      } else {
+        alert("Vui lòng nhập đầy đủ thông tin !");
+      }
+      if (invalid) {
+        await $http.get(apiUrl).then((res) => {
+          if (res.status == 200) {
+            const user = res.data.find(
+              (user) =>
+                $scope.user.username === user.username &&
+                $scope.user.password === user.password
+            );
+            if (user) {
+              let data = res.data[0];
+              delete data.password;
+              localStorage.setItem("user", JSON.stringify(data));
+              alert("Đăng nhập thành công !");
+              $location.path("/");
+            } else {
+              alert("Email hoặc mật khẩu không chính xác !");
+              return;
+            }
           }
-        }
-      });
+        });
+      }
     } catch (error) {
       alert(JSON.stringify(error));
     }
--- views/register/registerController.js
+++ views/register/registerController.js
@@ -15,29 +15,42 @@ myApp.controller("RegisterController", function ($scope, $http, $location) {
 
   $scope.handleRegister = async () => {
     try {
-      let response = await $http.get(apiUrl);
-      const isEmail = response.data.find(
-        (user) => $scope.user.email === user.email
-      );
-      const isNumberPhone = response.data.find(
-        (user) => $scope.user.numberPhone === user.numberPhone
-      );
-      if (isEmail) {
-        alert("Email đã tồn tại !");
-        return;
+      var invalid = false;
+      if (
+        $scope.user.fullName != "" &&
+        $scope.user.numberPhone != "" &&
+        $scope.user.email != "" &&
+        $scope.user.password != ""
+      ) {
+        invalid = true;
+      } else {
+        alert("Vui lòng nhập đầy đủ thông tin !");
       }
-      if (isNumberPhone) {
-        alert("Số điện thoại đã tồn tại !");
-        return;
-      }
-      const result = response.data;
-      $scope.user.id = result.length + 1;
-      await $http.post(apiUrl, $scope.user).then((res) => {
-        if (res.status == 201) {
-          alert("Đăng ký thành công !");
-          $location.path("/login");
+      if (invalid) {
+        let response = await $http.get(apiUrl);
+        const isEmail = response.data.find(
+          (user) => $scope.user.email === user.email
+        );
+        const isNumberPhone = response.data.find(
+          (user) => $scope.user.numberPhone === user.numberPhone
+        );
+        if (isEmail) {
+          alert("Email đã tồn tại !");
+          return;
         }
-      });
+        if (isNumberPhone) {
+          alert("Số điện thoại đã tồn tại !");
+          return;
+        }
+        const result = response.data;
+        $scope.user.id = result.length + 1;
+        await $http.post(apiUrl, $scope.user).then((res) => {
+          if (res.status == 201) {
+            alert("Đăng ký thành công !");
+            $location.path("/login");
+          }
+        });
+      }
     } catch (error) {
       alert(JSON.stringify(error));
     }
